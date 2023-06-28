893a070ab97989036e6bff4f2460e5db6addb13f
Michel <106674127+MichelZero@users.noreply.github.com>
2023-06-28 08:31:43 -0300

    up

--- ex01.py
+++ ex01.py
@@ -14,32 +14,34 @@
 
 # 1 ano mercuriano = 0.2408467 anos terrestres (365.26 / 87.97)
 # 1 ano marciano = 1.8808158 anos terrestres (365.26 / 686.98)
-# ex: idade em marte = idade em anos terrestres * 1.8808158
+# ex: idade em marte = idade em anos terrestres / 1.8808158
 
 class calcIdade:
     def __init__(self, idade):
         self.idade = idade
 
     def mercurio(self):
-        return self.idade * 0.2408467
+        # return self.idade * 0.2408467 # idade em anos mercurianos
+        return self.idade / 0.2408467 # idade em anos terrestres
 
     def venus(self):
-        return self.idade * 0.61519726
+        return self.idade / 0.61519726
 
     def marte(self):
-        return self.idade * 1.8808158
+        # return self.idade * 1.8808158 
+        return self.idade / 1.8808158
 
     def jupiter(self):
-        return self.idade * 11.862615
+        return self.idade / 11.862615
 
     def saturno(self):
-        return self.idade * 29.447498
+        return self.idade / 29.447498
 
     def urano(self):
-        return self.idade * 84.016846
+        return self.idade / 84.016846
 
     def netuno(self):
-        return self.idade * 164.79132
+        return self.idade / 164.79132
 
     def plutao(self):
-        return self.idade * 248.00
+        return self.idade / 248.00
---
+++ main.py
@@ -0,0 +1,14 @@
+#
+#
+# autores: Michel
+
+# data: 28/06/2023
+#
+# importando as classes
+from ex01 import calcIdade
+from ex02 import calcIdade2
+
+# criando objeto da classe calcIdade e passando a idade em anos 
+# terrestres como parâmetro para o construtor da classe calcIdade
+israel = calcIdade(10)
+print(f"em marte Israel têm a idade de {israel.marte()} anos")
